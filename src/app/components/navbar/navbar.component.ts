23a0a98324b8400c6a6408502e294472c71bf4c8
Brinkley Hill <bhill100@liberty.edu>
2022-09-17 12:18:32 -0400

    yuhhh

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,25 +1,25 @@
-import { Component, Input } from '@angular/core'
+import { Component, Input } from "@angular/core";
 
 @Component({
-  selector: 'app-navbar',
-  templateUrl: 'navbar.component.html',
-  styleUrls: ['navbar.component.css'],
+  selector: "app-navbar",
+  templateUrl: "navbar.component.html",
+  styleUrls: ["navbar.component.css"],
 })
 export class Navbar {
   @Input()
-  image_src: string = '/playground_assets/logo-1500h.png'
+  image_src: string = "/playground_assets/conecept-logo-01.png";
   @Input()
-  image_src1: string = '/playground_assets/logo-1500h.png'
+  image_src1: string = "/playground_assets/conecept-logo-01.png";
   @Input()
-  image_alt: string = 'logo'
+  image_alt: string = "logo";
   @Input()
-  image_alt1: string = 'image'
+  image_alt1: string = "image";
   @Input()
-  text: string = 'Follow us'
+  text: string = "Follow us";
   @Input()
-  PrimaryBtn: string = 'Get started'
+  PrimaryBtn: string = "Call us: (434) 434-4738";
   @Input()
-  PrimaryBtn1: string = 'Get started'
+  PrimaryBtn1: string = "Get started";
 
   constructor() {}
 }
--- src/app/components/navigation-links/navigation-links.component.ts
+++ src/app/components/navigation-links/navigation-links.component.ts
@@ -1,23 +1,23 @@
-import { Component, Input } from '@angular/core'
+import { Component, Input } from "@angular/core";
 
 @Component({
-  selector: 'navigation-links',
-  templateUrl: 'navigation-links.component.html',
-  styleUrls: ['navigation-links.component.css'],
+  selector: "navigation-links",
+  templateUrl: "navigation-links.component.html",
+  styleUrls: ["navigation-links.component.css"],
 })
 export class NavigationLinks {
   @Input()
-  text2: string = 'Features'
+  text2: string = "Location";
   @Input()
-  text3: string = 'Blog'
+  text3: string = "About";
   @Input()
-  rootClassName: string = ''
+  rootClassName: string = "";
   @Input()
-  text4: string = 'Pricing'
+  text4: string = "FAQ";
   @Input()
-  text: string = 'Home'
+  text: string = "Home";
   @Input()
-  text1: string = 'About'
+  text1: string = "Services";
 
   constructor() {}
 }
