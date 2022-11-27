c4805b9a99b52cdf0382416e1526f413a17594da
Brinkley Hill <8brink@gmail.com>
2022-11-27 15:09:59 -0500

    a mobile menu finally

--- src/app/components/navigation-links/navigation-links.component.ts
+++ src/app/components/navigation-links/navigation-links.component.ts
@@ -1,11 +1,17 @@
-import { Component, Input } from "@angular/core";
+import {
+  Component,
+  Input,
+} from "@angular/core";
 import { ViewportScroller } from "@angular/common";
 import { Router } from "@angular/router";
 
 @Component({
   selector: "navigation-links",
-  templateUrl: "navigation-links.component.html",
-  styleUrls: ["navigation-links.component.css"],
+  templateUrl:
+    "navigation-links.component.html",
+  styleUrls: [
+    "navigation-links.component.css",
+  ],
 })
 export class NavigationLinks {
   @Input()
@@ -21,44 +27,72 @@ export class NavigationLinks {
   @Input()
   text1: string = "Services";
 
-  constructor(private scroller: ViewportScroller, private router: Router) {}
+  constructor(
+    private scroller: ViewportScroller,
+    private router: Router
+  ) {}
   ngOnInit() {
     this.router.navigate(["/"]);
   }
 
   services() {
     //this.scroller.scrollToAnchor("targetGreen");
-    document.getElementById("services").scrollIntoView({
-      behavior: "smooth",
-      block: "start",
-      inline: "nearest",
-    });
+    document
+      .getElementById("checkbox")
+      .click();
+
+    document
+      .getElementById("services")
+      .scrollIntoView({
+        behavior: "smooth",
+        block: "start",
+        inline: "nearest",
+      });
   }
 
   location() {
     //this.scroller.scrollToAnchor("targetGreen");
-    document.getElementById("location").scrollIntoView({
-      behavior: "smooth",
-      block: "start",
-      inline: "nearest",
-    });
+    document
+      .getElementById("checkbox")
+      .click();
+    document
+      .getElementById("location")
+      .scrollIntoView({
+        behavior: "smooth",
+        block: "start",
+        inline: "nearest",
+      });
   }
 
   about() {
     //this.scroller.scrollToAnchor("targetGreen");
-    document.getElementById("about").scrollIntoView({
-      behavior: "smooth",
-      block: "start",
-      inline: "nearest",
-    });
+
+    document
+      .getElementById("checkbox")
+      .click();
+
+    document
+      .getElementById("about")
+      .scrollIntoView({
+        behavior: "smooth",
+        block: "start",
+        inline: "nearest",
+      });
   }
 
   faq() {
     //this.scroller.scrollToAnchor("targetGreen");
-    document.getElementById("faq").scrollIntoView({
-      behavior: "smooth",
-      block: "start",
-      inline: "nearest",
-    });
+
+    document
+      .getElementById("checkbox")
+      .click();
+
+    document
+      .getElementById("faq")
+      .scrollIntoView({
+        behavior: "smooth",
+        block: "start",
+        inline: "nearest",
+      });
   }
 }
