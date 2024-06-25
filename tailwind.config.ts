131598da9289d9f2856c5596d773b639ade0cde5
Brinkley Hill <8brink@gmail.com>
2024-06-25 12:07:01 -0400

    push

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,6 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 import defaultTheme from "tailwindcss/defaultTheme";
 
+
+
 export default {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
@@ -13,6 +15,7 @@ export default {
       backgroundImage: {
         'custom-image': 'url("/background.svg")',
       },
+
 
     },
   },
